30fef1cb380c488e27fce565dfa47cf6c516d02f
jayson <1344173512@qq.com>
2019-11-11 17:19:42 +0800

    线上测试用例--jayson20191111

--- case/personal_center/refund/test_refund.py
+++ case/personal_center/refund/test_refund.py
@@ -2,26 +2,26 @@ import unittest
 from common import base, logger, get_token
 from data.readexcel import ExcelUtil
 
-data = ExcelUtil("activity").dict_data()
+data = ExcelUtil("personal_center").dict_data()
 class Refund(unittest.TestCase):
 	'''测试测试一键返水'''
 	def setUp(self):
 		self.log = logger.Log()
 		self.token = get_token.GetRes().get_res()
 	def test_refund(self):
-		route = data[0]["route"]
+		route = data[29]["route"]
 		url = "".join(base.get_url(route))
-		Method = data[0]["method"]
-		json_data = eval(data[0]["data"])
+		Method = data[29]["method"]
+		params = eval(data[29]["params"])
 		headers = {"Authorization":self.token}
-		kwargs = {"data":json_data,"headers": headers}
+		kwargs = {"params":params,"headers": headers}
 		resp = base.get_response(url, Method, **kwargs)
 		self.log.info("----------test is start----------")
 		self.log.info("请求的接口地址为: %s" % url)
 		self.log.info("请求的参数为: %s" % kwargs)
 		self.log.info("响应内容为: %s" % resp.text)
 		self.log.info("响应状态码为: %s" % resp.status_code)
-		self.assertEqual(int(data[0]["expect"]), len(resp.json()["data"]), msg="失败原因为%s != %s" % (int(data[0]["expect"]), len(resp.json()["data"]))) # 断言优惠活动总数量
+		self.assertEqual(data[29]["expect"],resp.json()["message"], msg="失败原因为%s != %s" % (data[29]["expect"], resp.json()["message"]))
 		self.log.info("----------test is pass----------")
 		self.log.info("----------test is end----------")
 
