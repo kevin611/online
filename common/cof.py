e1a7dfa44661e421919e6e04541ad8b24cd244bd
jayson <1344173512@qq.com>
2019-11-11 16:28:06 +0800

    线上测试用例--jayson20191111

--- common/cof.py
+++ common/cof.py
@@ -1,14 +1,11 @@
 
 
 def get_host():
-    host = "http://klk.9161252.com:801" # 客拉客 测试会员账号mmmlll
-    host2 = "http://eyc.9161252.com:801" # 103棋牌 测试会员账号mmmlll
     host_pjdc = "http://pjdc.9161252.com" # 线上环境pjdc
     return host_pjdc
 
 
 # 获取验证码URL
 def get_codeurl():
-    code_url1 = "http://klk.9161252.com/frontend/v1/captcha?userName=mmmlll"
-    code_url2 = "http://eyc.9161252.com/frontend/v1/captcha?userName=mmmlll"
-    return code_url2
+    code_url = "http://pjdc.9161252.com/frontend/v1/captcha?userName=mmmlll"
+    return code_url
