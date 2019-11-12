b7768061e953b5fe90bb8298514b4e4cddc45b2a
jayson <1344173512@qq.com>
2019-11-12 14:35:35 +0800

    线上测试用例--jayson20191112

---
+++ case/bet_record/lottery/this_month/test_daily_xgsm_details.py
@@ -0,0 +1,34 @@
+import unittest
+from common import base, logger,get_token
+from data.readexcel import ExcelUtil
+from case.bet_record.lottery.this_month import get_this_month_list
+
+data = ExcelUtil("betrecord").dict_data()
+class BetRecordXgsm(unittest.TestCase):
+	def setUp(self):
+		self.log = logger.Log()
+	def test_xgsm_bet_details_daily(self):
+		'''测试获取本月每天的香港赛马投注详情'''
+		route = data[50]["route"]
+		url = "".join(base.get_url(route))
+		Method = data[50]["method"]
+		params = eval(data[50]["params"])
+		res = get_this_month_list.BetRecord().get_betrecord_list().json()["data"]["list"]  # 获取查询时间
+		for d in res:
+			date = d["date"]["date"]
+			params["date"] = date
+			headers = {"Authorization": get_token.GetRes().get_res()}
+			kwargs = {"params": params, "headers": headers}
+			resp = base.get_response(url,Method,**kwargs)
+			self.log.info("----------test is start----------")
+			self.log.info("请求的接口地址为: %s" % resp.url)
+			self.log.info("请求的参数为: %s" % kwargs)
+			self.log.info("响应内容为: %s" % resp.json())
+			self.log.info("响应状态码为: %s" % resp.status_code)
+			self.assertEqual(data[50]["expect"],resp.json()["message"],msg="失败原因为%s != %s" % ( data[50]["expect"],resp.json()["message"]))
+			self.log.info("----------test is pass----------")
+			self.log.info("----------test is end----------")
+
+
+if __name__ == "__main__":
+	unittest.main()
