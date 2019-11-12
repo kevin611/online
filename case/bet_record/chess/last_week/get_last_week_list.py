746b1fe5b1371638bb5081ce8610c29ea9240d5d
jayson <1344173512@qq.com>
2019-11-12 19:37:41 +0800

    线上测试用例--jayson20191112

---
+++ case/bet_record/chess/last_week/get_last_week_list.py
@@ -0,0 +1,25 @@
+import datetime
+from common import base,get_token
+from data.readexcel import ExcelUtil
+
+
+data = ExcelUtil("betrecord").dict_data()
+class BetRecord():
+	def __init__(self):
+		self.token = get_token.GetRes().get_res()
+
+	def get_betrecord_list(self):
+		'''获取上周棋牌注单列表'''
+		route = data[219]["route"]
+		url = "".join(base.get_url(route))
+		Method = data[219]["method"]
+		params = eval(data[219]["params"])
+		today = datetime.date.today()
+		params["time_start"] = today - datetime.timedelta(days=today.weekday() + 7)  # 上周一，开始时间
+		params["time_end"] = today - datetime.timedelta(days=today.weekday() + 1)  # 上周日，结束时间
+		headers = {"Authorization": self.token}
+		kwargs = {"params": params, "headers": headers}
+		resp = base.get_response(url, Method, **kwargs)
+		return resp
+
+
---
+++ case/bet_record/chess/this_week/test_daily_LEG_details.py
@@ -0,0 +1,37 @@
+import unittest
+from common import base, logger,get_token
+from data.readexcel import ExcelUtil
+from case.bet_record.chess.this_week import get_this_week_list
+
+
+
+data = ExcelUtil("betrecord").dict_data()
+class BetRecordLEG(unittest.TestCase):
+	def setUp(self):
+		# self.token = get_token.GetRes().get_res()
+		self.log = logger.Log()
+	def test_LEG_bet_details_daily(self):
+		'''测试获取本周每天的乐游棋牌投注详情'''
+		route = data[215]["route"]
+		url = "".join(base.get_url(route))
+		Method = data[215]["method"]
+		params = eval(data[215]["params"])
+		res = get_this_week_list.BetRecord().get_betrecord_list().json()["data"]["list"]
+		for d in res:
+			date = d["date"]["date"]
+			params["date"] = date
+			headers = {"Authorization": get_token.GetRes().get_res()}
+			kwargs = {"params": params, "headers": headers}
+			resp = base.get_response(url,Method,**kwargs)
+			self.log.info("----------test is start----------")
+			self.log.info("请求的接口地址为: %s" % resp.url)
+			self.log.info("请求的参数为: %s" % kwargs)
+			self.log.info("响应内容为: %s" % resp.json())
+			self.log.info("响应状态码为: %s" % resp.status_code)
+			self.assertEqual(data[215]["expect"],resp.json()["message"],msg="失败原因为%s != %s" % ( data[215]["expect"],resp.json()["message"]))
+			self.log.info("----------test is pass----------")
+			self.log.info("----------test is end----------")
+
+
+if __name__ == "__main__":
+	unittest.main()
