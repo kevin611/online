194a9cfb45ac29417e7b2c007f3b63cfa1e89a3b
jayson <1344173512@qq.com>
2019-11-12 19:03:33 +0800

    线上测试用例--jayson20191112

---
+++ case/bet_record/sport/last_month/get_last_month_list.py
@@ -0,0 +1,34 @@
+import datetime
+from common import base,get_token
+from data.readexcel import ExcelUtil
+
+
+data = ExcelUtil("betrecord").dict_data()
+class BetRecord():
+	def __init__(self):
+		self.token = get_token.GetRes().get_res()
+	def get_betrecord_list(self):
+		'''获取上月体育注单列表'''
+		route = data[206]["route"]
+		url = "".join(base.get_url(route))
+		Method = data[206]["method"]
+		params = eval(data[206]["params"])
+		date = datetime.datetime.now()
+		year = date.year
+		month = date.month
+		if month == 1:
+			start = datetime.date(year - 1, 12, 1)
+		else:
+			start = datetime.date(year, month - 1, 1)
+		end = datetime.date(year, month, 1) - datetime.timedelta(days=1)
+		params["time_start"] = start
+		params["time_end"] = end
+
+		params["time_start"] = start
+		params["time_end"] = end
+		headers = {"Authorization": self.token}
+		kwargs = {"params": params, "headers": headers}
+		resp = base.get_response(url, Method, **kwargs)
+		return resp
+
+
---
+++ case/bet_record/sport/last_month/test_get_last_month_lottery_betrecord_list.py
@@ -0,0 +1,24 @@
+import unittest
+from common import base, logger
+from data.readexcel import ExcelUtil
+from case.bet_record.sport.last_month import get_last_month_list
+
+
+data = ExcelUtil("betrecord").dict_data()
+class BetRecordList(unittest.TestCase):
+	'''测试获取体育上月投注记录'''
+	def setUp(self):
+		self.log = logger.Log()
+
+	def test_get_lotterylist_this_week(self):
+		res = get_last_month_list.BetRecord().get_betrecord_list() # 上月体育投注记录
+		self.log.info("----------test is start----------")
+		self.log.info("请求的接口地址为 %s" %res.url)
+		self.log.info("响应内容为: %s" % res.json())
+		self.log.info("响应状态码为: %s" % res.status_code)
+		self.assertEqual(data[206]["expect"],res.json()["message"],msg="失败原因%s ！= %s"%(data[206]["expect"],res.json()["message"]))
+		self.log.info("----------test is pass----------")
+		self.log.info("----------test is end----------")
+
+if __name__ == "__main__":
+	unittest.main()
