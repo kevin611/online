6151c165678015fb0bcad0b9b3351afa9949fffd
jayson <1344173512@qq.com>
2019-11-12 11:57:49 +0800

    线上测试用例--jayson20191112

---
+++ case/bet_record/lottery/this_week/get_this_week_list.py
@@ -0,0 +1,24 @@
+import datetime
+from common import base,get_token
+from data.readexcel import ExcelUtil
+
+
+data = ExcelUtil("betrecord").dict_data()
+class BetRecord():
+	def __init__(self):
+		self.token = get_token.GetRes().get_res()
+
+	def get_betrecord_list(self):
+		'''获取本周彩票注单列表'''
+		route = data[0]["route"]
+		url = "".join(base.get_url(route))
+		Method = data[0]["method"]
+		params = eval(data[0]["params"])
+		today = datetime.date.today()
+		params["time_start"] = today - datetime.timedelta(days=today.weekday())  # 周一，开始时间
+		params["time_end"] = today  # 今天，结束时间
+		headers = {"Authorization": self.token}
+		kwargs = {"params": params, "headers": headers}
+		resp = base.get_response(url, Method, **kwargs)
+		return resp
+
